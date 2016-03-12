8d87728120efb49038b18355e26b34271d13b1f4
David Roberts <david.roberts63@gmail.com>
2016-03-11 23:17:38 -0600

    Chuztpah for jasmine tests

--- Tests/lib/SpecRunner.js
+++ Tests/lib/SpecRunner.js
@@ -1,13 +1,19 @@
 
 if (navigator.userAgent.indexOf("PhantomJS") > 0) {
-  var consoleReporter = new jasmineRequire.ConsoleReporter()({
-    showColors: false,
-    timer: new jasmine.Timer,
-    print: function() {
-      console.log.apply(console, arguments)
-    }
-  });
+    //if (jasmineReporters) {
+    //    console.log("Setting up jasmine reporter for PhantomJS");
+    //    var junitReporter = new jasmineReporters.TapReporter();
+    //    jasmine.getEnv().addReporter(junitReporter);
+    //} else {
+    //    var consoleReporter = new jasmineRequire.ConsoleReporter()({
+    //        showColors: false,
+    //        timer: new jasmine.Timer,
+    //        print: function () {
+    //            console.log.apply(console, arguments)
+    //        }
+    //    });
 
-  jasmine.getEnv().addReporter(consoleReporter);
+    //    jasmine.getEnv().addReporter(consoleReporter);
+    //}
 }
 
